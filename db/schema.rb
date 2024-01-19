f3ea7b5ada4c634725877d8cee7b176e04a98394
me <myemail@example.com>
2024-01-19 20:41:23 +0900

    add: itemsテーブルにdetailカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_01_065120) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_17_124127) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_01_065120) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
+    t.text "detail"
   end
 
   create_table "tasks", force: :cascade do |t|
