cce24048fa2dfc122373c55e51729f6441d9b56f
me <myemail@example.com>
2024-01-22 18:57:30 +0900

    feat: デモデータ用の画像をActiveStorageで保存

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,41 +12,41 @@ Rails.logger.debug '==================== item create ===================='
 item1 = Item.new(
   name: 'イチゴショートケーキ',
   price: 150,
-  image_name: 'strawberry-short-cake.jpg',
   detail: 'シャンテリークリームに苺、スポンジをあわせたスタンダードなショートケーキ。大切な記念日におすすめです。'
 )
+item1.image.attach(io: File.open(Rails.root.join('app/assets/images/item/strawberry-short-cake.jpg')), filename: 'strawberry-short-cake.jpg')
 item1.save
 
 item2 = Item.new(
   name: 'チョコレートショートケーキ',
   price: 180,
-  image_name: 'chocolate-short-cake.jpg',
   detail: 'なめらかなチョコレートクリームとソフトなチョコスポンジ、サンドしたチョコチップをお楽しみください。'
 )
+item2.image.attach(io: File.open(Rails.root.join('app/assets/images/item/chocolate-short-cake.jpg')), filename: 'chocolate-short-cake.jpg')
 item2.save
 
 item3 = Item.new(
   name: 'モンブラン',
   price: 150,
-  image_name: 'mont-blanc.jpg',
   detail: '蒸し栗ペーストを入れ、栗のおいしさをとじこめました。黄色いマロンクリーム、シャンテリークリーム、スポンジの一体感が楽しめるモンブランです。'
 )
+item3.image.attach(io: File.open(Rails.root.join('app/assets/images/item/mont-blanc.jpg')), filename: 'mont-blanc.jpg')
 item3.save
 
 item4 = Item.new(
   name: '抹茶ロール',
   price: 250,
-  image_name: 'green-tea-cake-roll.jpg',
   detail: '鹿児島県産一番茶抹茶使用。抹茶スポンジに抹茶味のクリームとガナッシュ、大納言かのこをサンドした抹茶好きにはたまらない贅沢な抹茶です。'
 )
+item4.image.attach(io: File.open(Rails.root.join('app/assets/images/item/green-tea-cake-roll.jpg')), filename: 'green-tea-cake-roll.jpg')
 item4.save
 
 item5 = Item.new(
   name: 'チーズケーキ',
   price: 150,
-  image_name: 'cheese-cake.jpg',
   detail: 'デンマーク産のクリームチーズを使用した、ふんわりとした口どけが特長の濃厚なスフレチーズケーキです。'
 )
+item5.image.attach(io: File.open(Rails.root.join('app/assets/images/item/cheese-cake.jpg')), filename: 'cheese-cake.jpg')
 item5.save
 
 item6 = Item.new(
@@ -55,20 +55,21 @@ item6 = Item.new(
   image_name: 'blueberry-torte.jpg',
   detail: 'リッチなコクのあるタルト台にブルーベリーをたっぷり飾り、ブルーベリーソースで仕上げました。'
 )
+item6.image.attach(io: File.open(Rails.root.join('app/assets/images/item/blueberry-torte.jpg')), filename: 'blueberry-torte.jpg')
 item6.save
 
 item7 = Item.new(
   name: 'ミルフィーユ',
   price: 100,
-  image_name: 'millfeuile.jpg',
   detail: 'ミルフィーユ板に、バターとカスタードクリームを合わせた「ムスリーヌクリーム」をサンドし、サクサク食感のフィアンティーヌがアクセント。'
 )
+item7.image.attach(io: File.open(Rails.root.join('app/assets/images/item/millfeuile.jpg')), filename: 'millfeuile.jpg')
 item7.save
 
 item8 = Item.new(
   name: 'イチゴタルト',
   price: 130,
-  image_name: 'strawberry-torte.jpg',
   detail: 'タルト台に苺をふんだんに飾った、苺が主役のタルトです。'
 )
+item8.image.attach(io: File.open(Rails.root.join('public/item_images/strawberry-torte.jpg')), filename: 'strawberry-torte.jpg')
 item8.save
