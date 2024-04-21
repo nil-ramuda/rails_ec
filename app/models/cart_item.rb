db1864b79fd583433c1439a8f13653936f28bcd2
ramuda <myemail@example.com>
2024-04-21 17:56:54 +0900

    refactor: RuboCopを適用

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,7 +3,8 @@
 class CartItem < ApplicationRecord
   belongs_to :item
   belongs_to :cart
-  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 99 }
+  validates :quantity, presence: true,
+                       numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 99 }
   validates :cart_id, presence: true
   validates :item_id, presence: true
 end
