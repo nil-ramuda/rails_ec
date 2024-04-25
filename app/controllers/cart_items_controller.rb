3be8b35dffbcf31494ff45ccfd3cc65f2c13774d
ramuda <myemail@example.com>
2024-04-26 08:31:45 +0900

    refactor: 商品詳細画面で未追加の商品を追加するときの処理をリファクタ

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -11,19 +11,21 @@ class CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = @cart.cart_items.find_or_initialize_by(cart_item_params)
-    if @cart_item.quantity.nil?
-      @cart_item.quantity = params[:quantity]
-    else
-      @cart_item.quantity += params[:quantity].to_i
-    end
+    @cart_item = @cart.cart_items.create(cart_item_params)
+    flash[:notice] = "カートに#{@cart_item.item.name}を#{params[:quantity].to_i}個追加しました"
+    redirect_to action: params[:action], controller: params[:controller]
+  end
+
+  def update
+    @cart_item = @cart.cart_items.find(params[:id])
+    @cart_item.quantity += params[:quantity].to_i
     @cart_item.save
     flash[:notice] = "カートに#{@cart_item.item.name}を#{params[:quantity].to_i}個追加しました"
     redirect_to action: params[:action], controller: params[:controller]
   end
 
   def destroy
-    @cart_item = @cart.cart_items.find_by(item_id: params[:item_id], cart_id: session[:cart_id])
+    @cart_item = @cart.cart_items.find_by(item_id: params[:item_id])
     @cart_item.destroy
     flash[:notice] = "カートから#{@cart_item.item.name}を削除しました"
     redirect_to checkout_index_path
@@ -33,16 +35,14 @@ class CartItemsController < ApplicationController
 
   def set_cart
     @total_price = 0
-    if Cart.find_by(id: session[:cart_id])
-      @cart = Cart.find_by(id: session[:cart_id])
-    else
-      @cart = Cart.new
-      @cart.save
+    @cart = Cart.find_by(id: session[:cart_id])
+    if @cart.nil?
+      @cart = Cart.create
       session[:cart_id] = @cart.id
     end
   end
 
   def cart_item_params
-    params.permit(:item_id)
+    params.permit(:quantity, :item_id)
   end
 end
