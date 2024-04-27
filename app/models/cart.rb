eb353cb248a3312043b88b6ef500bd7023145a5e
ramuda <myemail@example.com>
2024-04-27 14:24:44 +0900

    style: コメントアウトの行を削除

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,5 +2,4 @@
 
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
-  # has_many :items, through: :cart_items, dependent: :destroy
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,7 @@
 
 class Item < ApplicationRecord
   has_one_attached :image
-  # has_many :cart_item
   has_many :cart_items, dependent: :destroy
-  # has_many :cart, through: :cart_item
-  # has_many :carts, through: :cart_items, dependent: :destroy
   validates :name, presence: true, length: { maximum: 20 }
   validates :price, presence: true, numericality: { less_than_or_equal_to: 1000 }
   validates :detail, presence: true, length: { maximum: 100 }
