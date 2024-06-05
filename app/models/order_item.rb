17dda41bd46abf1bff6a2b65f9f8f000c2593b9d
ramuda <myemail@example.com>
2024-06-05 20:05:26 +0900

    add: バリデーションを追加

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,9 @@
 
 class OrderItem < ApplicationRecord
   belongs_to :order
+  with_options presence: true do
+    validates :name, length: { maximum: 20 }
+    validates :price, numericality: { less_than_or_equal_to: 1000 }
+    validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 99 }
+  end
 end
